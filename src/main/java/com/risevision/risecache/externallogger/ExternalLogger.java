87f5f5302bf01cda6b7e98aa7a28a6de3dac24cf
rodrigopavezi <rodrigopavezi@gmail.com>
2016-02-26 16:21:04 -0300

    Removed the use of the Log utils on the ExternalLogger. It was looping indefinitely.

--- src/main/java/com/risevision/risecache/externallogger/ExternalLogger.java
+++ src/main/java/com/risevision/risecache/externallogger/ExternalLogger.java
@@ -1,35 +1,36 @@
 package com.risevision.risecache.externallogger;
 
-import com.risevision.risecache.Log;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.logging.Logger;
 
 public class ExternalLogger {
   private static final String logUrl = "https://www.googleapis.com/bigquery/v2/" +
   "projects/client-side-events/datasets/Rise_Cache/tables/events/insertAll";
+  private static Logger log = Logger.getLogger("ExternalLogger");
 
   public static void logExternal(InsertSchema schema) {
     try {
       Token.update();
     } catch (IOException e) {
-      Log.error("External logger token update", e.getMessage());
+      log.warning("External logger token update " + e.getMessage());
     }
 
     if (schema.getEvent() == null || schema.getEvent().equals("")) {
       throw new RuntimeException("No event specified");
     }
 
-    
+
     byte[] json = schema.setInsertId().setTimestamp().getJson().getBytes();
 
     try {
       HttpURLConnection conn = (HttpURLConnection) getUrl().openConnection();
       conn.setRequestMethod("POST");
       conn.setDoOutput(true);
-      conn.setRequestProperty("Content-Type", "application/json"); 
+      conn.setRequestProperty("Content-Type", "application/json");
       conn.setRequestProperty("Content-Length", String.valueOf(json.length));
       conn.setRequestProperty("Authorization", "Bearer " + Token.token);
       try (DataOutputStream out = new DataOutputStream(conn.getOutputStream())) {
@@ -37,7 +38,7 @@ public class ExternalLogger {
       }
       conn.getInputStream().close();
     } catch (IOException e) {
-      Log.error("External logger event save", e.getMessage());
+      log.warning("External logger event save " + e.getMessage());
     }
   }
 
